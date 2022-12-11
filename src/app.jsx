b19276770065ada0db313cd05538105c08f57065
cardogg <rdgonzalez@protonmail.com>
2022-12-11 04:31:05 -0800

    Changed to exercise 4 template

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+const USERNAME = 'rickardo@csu.fullerton.edu'
+const PASSWORD = 'password1.'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Hello, {authData.admin.email}</h1>
+    </div>
+  )
+}
