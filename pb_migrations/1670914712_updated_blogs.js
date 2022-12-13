7ab1af877fe807425a9f731c14e2cea411508a15
Daisy Catalan <dcatalan@csu.fullerton.edu>
2022-12-13 00:01:57 -0800

    define initial collections

---
+++ pb_migrations/1670914712_updated_blogs.js
@@ -0,0 +1,101 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("34a61ab4tqi3oep")
+
+  collection.name = "posts"
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zo4jgoiw",
+    "name": "author",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vtpbpmla",
+    "name": "headline",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "r606affe",
+    "name": "featured_image",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [
+        "image/jpg",
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/gif"
+      ],
+      "thumbs": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("34a61ab4tqi3oep")
+
+  collection.name = "blogs"
+
+  // remove
+  collection.schema.removeField("zo4jgoiw")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vtpbpmla",
+    "name": "title",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "r606affe",
+    "name": "file",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": []
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
