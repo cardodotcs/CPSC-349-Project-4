c9921c7f58ba101bf7ae7dd837ac6f716c29f1b6
Daisy Catalan <dcatalan@csu.fullerton.edu>
2022-12-13 02:41:30 -0800

    my react tw stuff

--- pb_public/app.js
+++ pb_public/app.js
@@ -8,5 +8,102 @@ root.render(
   /* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null))
 );
 function App() {
-  return /* @__PURE__ */ React.createElement("div", { className: "App" }, /* @__PURE__ */ React.createElement("h1", null, "Hello, ", authData.admin.email));
+  return /* @__PURE__ */ React.createElement("div", { className: "App" }, /* @__PURE__ */ React.createElement("h1", { className: "text-3xl font-bold underline bg-green-500" }, "Hello, ", authData.admin.email), /* @__PURE__ */ React.createElement("header", { className: "text-gray-600 body-font" }, /* @__PURE__ */ React.createElement("div", { className: "container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center" }, /* @__PURE__ */ React.createElement("nav", { className: "flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto" }, /* @__PURE__ */ React.createElement(
+    "a",
+    {
+      className: "mr-5 hover:text-gray-900",
+      style: { color: "rgb(0, 119, 255)" },
+      href: "/"
+    },
+    "Home"
+  ), /* @__PURE__ */ React.createElement(
+    "a",
+    {
+      className: "mr-5 hover:text-gray-900",
+      style: { color: "rgb(0, 119, 255)" },
+      href: "about.html"
+    },
+    "About"
+  )), /* @__PURE__ */ React.createElement("a", { className: "flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0" }, /* @__PURE__ */ React.createElement(
+    "img",
+    {
+      src: "https://www.onblastblog.com/wp-content/uploads/2017/08/blogger-logo.jpg",
+      className: "w-12 h-12 text-white p-2 rounded-full",
+      alt: "logo"
+    }
+  ), /* @__PURE__ */ React.createElement("span", { className: "ml-3 text-xl", style: { color: "rgb(0, 119, 255)" } }, "Blog Engine")), /* @__PURE__ */ React.createElement("div", { className: "lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0" }, /* @__PURE__ */ React.createElement(
+    "a",
+    {
+      className: "inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0",
+      href: "login.html"
+    },
+    " ",
+    "Log in",
+    /* @__PURE__ */ React.createElement(
+      "svg",
+      {
+        fill: "none",
+        stroke: "currentColor",
+        strokeLinecap: "round",
+        strokeLinejoin: "round",
+        strokeWidth: 2,
+        className: "w-4 h-4 ml-1",
+        viewBox: "0 0 24 24"
+      },
+      /* @__PURE__ */ React.createElement("path", { d: "M5 12h14M12 5l7 7-7 7" })
+    )
+  )))), /* @__PURE__ */ React.createElement("section", { className: "text-gray-600 body-font" }, /* @__PURE__ */ React.createElement("div", { className: "container px-5 py-24 mx-auto" }, /* @__PURE__ */ React.createElement("h1", { className: "text-3xl font-medium title-font text-gray-900 mb-12 text-center" }, "Welcome to Blog Engine"), /* @__PURE__ */ React.createElement("div", { className: "flex flex-wrap -m-4" }, /* @__PURE__ */ React.createElement("div", { className: "p-4 md:w-1/2 w-full" }, /* @__PURE__ */ React.createElement("div", { className: "h-full bg-gray-100 p-8 rounded" }, /* @__PURE__ */ React.createElement("p", { className: "leading-relaxed mb-6" }, "What is a CPU? It stands for a Core Processing Unit, it does all the logical arthimetic and all the processing for onces computer. The CPU is reponsible for all functions that involve logic. There are many parts of a computer but CPU allows the computer to be able to perform its dail functions and it gets all the needed processes from RAM."), /* @__PURE__ */ React.createElement("a", { className: "inline-flex items-center" }, /* @__PURE__ */ React.createElement("span", { className: "flex-grow flex flex-col pl-4" }, /* @__PURE__ */ React.createElement("span", { className: "title-font font-medium text-gray-900" }, "cardo"))))), /* @__PURE__ */ React.createElement("div", { className: "p-4 md:w-1/2 w-full" }, /* @__PURE__ */ React.createElement("div", { className: "h-full bg-gray-100 p-8 rounded" }, /* @__PURE__ */ React.createElement("p", { className: "leading-relaxed mb-6" }, "Going off of the CPU blog from Pbavafa, RAM (random access memory) is a computer's short-term memory, where the data that the processor is currently using is stored. Your computer can access RAM memory much faster than data on a hard disk, SSD, or other long-term storage device, which is why RAM capacity is critical for system performance."), /* @__PURE__ */ React.createElement("a", { className: "inline-flex items-center" }, /* @__PURE__ */ React.createElement("span", { className: "flex-grow flex flex-col pl-4" }, /* @__PURE__ */ React.createElement("span", { className: "title-font font-medium text-gray-900" }, "sbhagat101")))))))), /* @__PURE__ */ React.createElement(
+    "footer",
+    {
+      className: "text-gray-600 body-font",
+      style: { position: "relative", top: "auto" }
+    },
+    /* @__PURE__ */ React.createElement("div", { className: "container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col" }, /* @__PURE__ */ React.createElement(
+      "div",
+      {
+        className: "w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10",
+        style: { position: "relative", top: 150 }
+      },
+      /* @__PURE__ */ React.createElement("a", { className: "flex title-font font-medium items-center md:justify-start justify-center text-gray-900" }, /* @__PURE__ */ React.createElement(
+        "img",
+        {
+          src: "https://www.onblastblog.com/wp-content/uploads/2017/08/blogger-logo.jpg",
+          className: "w-12 h-12 text-white p-2 rounded-full",
+          alt: "logo"
+        }
+      ), /* @__PURE__ */ React.createElement("span", { className: "ml-3 text-xl" }, "Blog Engine")),
+      /* @__PURE__ */ React.createElement("p", { className: "mt-2 text-sm text-gray-500" }, "The place to satisfy all your blogging needs")
+    ), /* @__PURE__ */ React.createElement(
+      "div",
+      {
+        className: "flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first",
+        style: { position: "relative", top: 150, right: "auto" }
+      },
+      /* @__PURE__ */ React.createElement("div", { className: "lg:w-1/4 md:w-1/2 w-full px-4" }, /* @__PURE__ */ React.createElement(
+        "h2",
+        {
+          className: "title-font font-medium text-gray-900 tracking-widest text-sm mb-3",
+          style: { fontSize: 20 }
+        },
+        "Links"
+      ), /* @__PURE__ */ React.createElement("nav", { className: "list-none mb-10" }, /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement("a", { style: { color: "rgb(0, 119, 255)" }, href: "./about.html" }, "About Us")), /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(
+        "a",
+        {
+          className: "text-gray-600 hover:text-gray-800",
+          style: { color: "rgb(0, 119, 255)" },
+          href: "https://tailblocks.cc/"
+        },
+        "Tailblocks"
+      )), /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(
+        "a",
+        {
+          className: "text-gray-600 hover:text-gray-800",
+          style: { color: "rgb(0, 119, 255)" },
+          href: "https://github.com/cardogg/CPSC-349-Project-4"
+        },
+        "Main Github Repo"
+      )))))
+    )),
+    /* @__PURE__ */ React.createElement("div", { className: "bg-gray-100", style: { position: "relative", top: 60 } }, /* @__PURE__ */ React.createElement("div", { className: "container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row" }, /* @__PURE__ */ React.createElement("p", { className: "text-gray-500 text-sm text-center sm:text-left" }, "\xA9 2022 Blog Engine")))
+  ));
 }
